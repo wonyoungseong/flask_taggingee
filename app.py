3c8f0f2a434526520ba441779251e6264d7017d6
wonyoungseong <94332623+wonyoungseong@users.noreply.github.com>
2023-11-19 17:27:17 +0900

    Update app.py

--- app.py
+++ app.py
@@ -67,85 +67,37 @@ def day_of_the_week():
 a = {}
 
 
-@app.route("/webhook/", methods=["GET", "POST"])
+@app.route("/webhook/", methods=["POST"])
 def webhook():
     global a
-    request_data = json.loads(request.get_data())
+    request_data = json.loads(request.get_data(), encoding='utf-8')
     a[request_data['user']] = request_data['result']['choices'][0]['message']['content']
     return 'OK'
 
 
-@app.route("/question", methods=["GET", "POST"])
+@app.route("/question", methods=["POST"])
 def get_question():
     global a
-    if request.method == "GET":
-        # GET 요청에 대한 처리
-        question = request.args.get('question')
-        return jsonify({"message": f"GET 요청 받음, 질문: {question}"})
-    elif request.method == "POST":
-        try:
-            request_data = json.loads(request.get_data())
-            # POST 요청 처리 로직
-            # ...
-
-            # 외부 API 요청
-            try:
-                api_response = requests.post('https://api.asyncia.com/v1/api/request/', json={
-                    # API 요청 관련 데이터
-                }, timeout=5)  # 타임아웃 값을 조정
-                # API 응답 처리 로직
-                # ...
-            except requests.exceptions.RequestException as e:
-                # 외부 API 요청 오류 처리
-                return jsonify({"error": "External API request failed"}), 500
-
-            # 성공적인 응답 반환
-            return jsonify({"message": "Success"})
-        except json.JSONDecodeError:
-            # JSON 파싱 오류 처리
-            return jsonify({"error": "Invalid JSON format"}), 400
-        except Exception as e:
-            # 기타 예외 처리
-            return jsonify({"error": "An error occurred"}), 500
+    request_data = json.loads(request.get_data(), encoding='utf-8')
+    response = {"version": "2.0", "template": {"outputs": [{
+        "simpleText": {"text": f"질문을 받았습니다. AI에게 물어보고 올께요!: {request_data['action']['params']['question']}"}
+    }]}}
+    a[request_data['userRequest']['user']['id']] = '아직 AI가 처리중이에요'
+    try:
+        api = requests.post('https://api.asyncia.com/v1/api/request/', json={
+            "apikey": "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",
+            "messages": [{"role": "user", "content": request_data['action']['params']['question']}],
+            "userdata": [["user", request_data['userRequest']['user']['id']]]},
+            headers={"apikey": "A0.cd6a467e-67e7-47a7-87fa-6524ac0fee92._lE5wodHE3xjzO0EMcoS8PV1732wXG-lvg"}, timeout=0.3)
+    except requests.exceptions.ReadTimeout:
+        pass
+    return jsonify(response)
 
 
-@app.route("/ans", methods=["GET", "POST"])
+@app.route("/ans", methods=["POST"])
 def hello2():
-    if request.method == "GET":
-        user_id = request.args.get('user_id')
-        response_text = a.get(user_id, '질문을 하신적이 없어보여요. 질문부터 해주세요')
-        response = {
-            "version": "2.0",
-            "template": {
-                "outputs": [
-                    {
-                        "simpleText": {
-                            "text": f"답변: {response_text}"
-                        }
-                    }
-                ]
-            }
-        }
-        return jsonify(response)
-    else:
-        # POST 요청에 대한 기존 처리를 유지
-        request_data = json.loads(request.get_data())
-        response_text = a.get(
-            request_data['userRequest']['user']['id'], '질문을 하신적이 없어보여요. 질문부터 해주세요')
-        response = {
-            "version": "2.0",
-            "template": {
-                "outputs": [
-                    {
-                        "simpleText": {
-                            "text": f"답변: {response_text}"
-                        }
-                    }
-                ]
-            }
-        }
-        return jsonify(response)
-
-
-if __name__ == "__main__":
-    app.run(host="0.0.0.0")
+    request_data = json.loads(request.get_data(), encoding='utf-8')
+    response = {"version": "2.0", "template": {"outputs": [{
+        "simpleText": {"text": f"답변: {a.get(request_data['userRequest']['user']['id'], '질문을 하신적이 없어보여요. 질문부터 해주세요')}"}
+    }]}}
+    return jsonify(response)
