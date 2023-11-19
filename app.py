388b1671f471530c444984e5d901dbead6b88193
wonyoungseong <94332623+wonyoungseong@users.noreply.github.com>
2023-11-19 22:33:53 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,19 +1,19 @@
 from flask import Flask, jsonify, request
 import requests, sys, json
-app = Flask(__name__)
+app= Flask(__name__)
 a = {}
 
 @app.route("/webhook/", methods=["POST"])
 def webhook():
     global a
-    request_data = json.loads(request.get_data())
+    request_data = json.loads(request.get_data(), encoding='utf-8')
     a[request_data['user']] = request_data['result']['choices'][0]['message']['content']
     return 'OK'
 
 @app.route("/question", methods=["POST"])
 def get_question():
     global a
-    request_data = json.loads(request.get_data())
+    request_data = json.loads(request.get_data(), encoding='utf-8')
     response = { "version": "2.0", "template": { "outputs": [{
         "simpleText": {"text": f"질문을 받았습니다. AI에게 물어보고 올께요!: {request_data['action']['params']['question']}"}
     }]}}
@@ -30,7 +30,7 @@ def get_question():
 
 @app.route("/ans", methods=["POST"])
 def hello2():
-    request_data = json.loads(request.get_data())
+    request_data = json.loads(request.get_data(), encoding='utf-8')
     response = { "version": "2.0", "template": { "outputs": [{
         "simpleText": {"text": f"답변: {a.get(request_data['userRequest']['user']['id'], '질문을 하신적이 없어보여요. 질문부터 해주세요')}"}
     }]}}
