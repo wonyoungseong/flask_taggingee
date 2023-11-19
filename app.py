ccab3079ee5eb0e5852f0e6aba7af6b43a1e7111
wonyoungseong <94332623+wonyoungseong@users.noreply.github.com>
2023-11-19 22:22:11 +0900

    Update app.py

--- app.py
+++ app.py
@@ -20,7 +20,7 @@ def get_question():
     a[request_data['userRequest']['user']['id']] = '아직 AI가 처리중이에요'
     try:
         api = requests.post('https://api.asyncia.com/v1/api/request/', json={
-            "apikey": "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",
+            "apikey": "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",
             "messages" :[{"role": "user", "content": request_data['action']['params']['question']}],
             "userdata": [["user", request_data['userRequest']['user']['id']]]},
             headers={"apikey":"A0.cd6a467e-67e7-47a7-87fa-6524ac0fee92._lE5wodHE3xjzO0EMcoS8PV1732wXG-lvg"}, timeout=0.3)
