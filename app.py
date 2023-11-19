0e76186b17cfdbafcfb29265b6cbe0c5317a7332
wonyoungseong <94332623+wonyoungseong@users.noreply.github.com>
2023-11-19 11:34:24 +0900

    Update app.py_test_mehtod
    root update
    methods update

--- app.py
+++ app.py
@@ -1,7 +1,30 @@
-from flask import Flask
+from flask import Flask, jsonify
 import sys
+import random
 app = Flask(__name__)
 
+# 루트 경로 ("/") 라우트
+
+
 @app.route("/")
-def hello():
-    return "Hello typeCloud"
+def home():
+    return "Welcome to the Home Page!"
+
+# "/random" 경로 라우트
+
+
+@app.route("/random", methods=["GET", "POST"])
+def random_function():
+    response = {
+        "version": "2.0",
+        "template": {
+            "outputs": [
+                {
+                    "simpleText": {
+                        "text": str(random.randint(1, 100))
+                    }
+                }
+            ]
+        }
+    }
+    return jsonify(response)
