9f8bffd85bc0f8c4972a0aa5c7df2f8fc2236a2c
wonyoungseong <94332623+wonyoungseong@users.noreply.github.com>
2023-11-19 13:16:43 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, jsonify, request
+import requests
 import sys
 import random
 import json
@@ -66,3 +67,41 @@ def day_of_the_week():
     else:
         # GET 요청에 대한 처리 (예: 간단한 안내 메시지 반환)
         return "Please use POST request to send data."
+
+a = {}
+
+
+@app.route("/webhook/", methods=["POST"])
+def webhook():
+    global a
+    request_data = json.loads(request.get_data(), encoding='utf-8')
+    a[request_data['user']] = request_data['result']['choices'][0]['message']['content']
+    return 'OK'
+
+
+@app.route("/question", methods=["POST"])
+def get_question():
+    global a
+    request_data = json.loads(request.get_data(), encoding='utf-8')
+    response = {"version": "2.0", "template": {"outputs": [{
+        "simpleText": {"text": f"질문을 받았습니다. AI에게 물어보고 올께요!: {request_data['action']['params']['question']}"}
+    }]}}
+    a[request_data['userRequest']['user']['id']] = '아직 AI가 처리중이에요'
+    try:
+        api = requests.post('https://api.asyncia.com/v1/api/request/', json={
+            "apikey": "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",
+            "messages": [{"role": "user", "content": request_data['action']['params']['question']}],
+            "userdata": [["user", request_data['userRequest']['user']['id']]]},
+            headers={"apikey": "A0.cd6a467e-67e7-47a7-87fa-6524ac0fee92._lE5wodHE3xjzO0EMcoS8PV1732wXG-lvg"}, timeout=0.3)
+    except requests.exceptions.ReadTimeout:
+        pass
+    return jsonify(response)
+
+
+@app.route("/ans", methods=["POST"])
+def hello2():
+    request_data = json.loads(request.get_data(), encoding='utf-8')
+    response = {"version": "2.0", "template": {"outputs": [{
+        "simpleText": {"text": f"답변: {a.get(request_data['userRequest']['user']['id'], '질문을 하신적이 없어보여요. 질문부터 해주세요')}"}
+    }]}}
+    return jsonify(response)
