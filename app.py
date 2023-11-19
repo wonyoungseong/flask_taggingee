a219e5ec47acfc5be1ddc92817585b0aa557d894
wonyoungseong <94332623+wonyoungseong@users.noreply.github.com>
2023-11-19 11:56:34 +0900

    method_update_app.py
    only post

--- app.py
+++ app.py
@@ -3,17 +3,7 @@ import sys
 import random
 app = Flask(__name__)
 
-# 루트 경로 ("/") 라우트
-
-
-@app.route("/")
-def home():
-    return "Welcome to the Home Page!"
-
-# "/random" 경로 라우트
-
-
-@app.route("/random", methods=["GET", "POST"])
+@app.route("/random", methods=["POST"])
 def random_function():
     response = {
         "version": "2.0",
